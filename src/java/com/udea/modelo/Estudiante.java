0e48d025d561d590de0d8f2d9a2707253ad0d127
Robot Alberto <David Mejia>
2020-09-29 00:09:20 -0500

    Creacion del modelo y el ejb

---
+++ src/java/com/udea/modelo/Estudiante.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.modelo;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author robot
+ */
+@Entity
+@Table(name = "estudiante")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Estudiante.findAll", query = "SELECT e FROM Estudiante e")
+    , @NamedQuery(name = "Estudiante.findById", query = "SELECT e FROM Estudiante e WHERE e.id = :id")
+    , @NamedQuery(name = "Estudiante.findByNombre", query = "SELECT e FROM Estudiante e WHERE e.nombre = :nombre")
+    , @NamedQuery(name = "Estudiante.findByCorreo", query = "SELECT e FROM Estudiante e WHERE e.correo = :correo")
+    , @NamedQuery(name = "Estudiante.findByContrase\u00f1a", query = "SELECT e FROM Estudiante e WHERE e.contrase\u00f1a = :contrase\u00f1a")
+    , @NamedQuery(name = "Estudiante.findByImagen", query = "SELECT e FROM Estudiante e WHERE e.imagen = :imagen")})
+public class Estudiante implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "correo")
+    private String correo;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "contrase\u00f1a")
+    private String contraseña;
+    @Size(max = 50)
+    @Column(name = "imagen")
+    private String imagen;
+    @JoinTable(name = "matricula", joinColumns = {
+        @JoinColumn(name = "idEstudiante", referencedColumnName = "id")}, inverseJoinColumns = {
+        @JoinColumn(name = "codigoMateria", referencedColumnName = "codigo")})
+    @ManyToMany
+    private Collection<Materia> materiaCollection;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(Integer id) {
+        this.id = id;
+    }
+
+    public Estudiante(Integer id, String nombre, String correo, String contraseña) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    @XmlTransient
+    public Collection<Materia> getMateriaCollection() {
+        return materiaCollection;
+    }
+
+    public void setMateriaCollection(Collection<Materia> materiaCollection) {
+        this.materiaCollection = materiaCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Estudiante)) {
+            return false;
+        }
+        Estudiante other = (Estudiante) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.udea.modelo.Estudiante[ id=" + id + " ]";
+    }
+    
+}
